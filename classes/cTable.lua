13bcbe751c737437cf0cf73ddf661da903f78eb1
bjorn-nesby <bjorn.nesby@gmail.com>
2018-06-27 16:54:08 +0200

    - cTable: new method - `is_indexed`

--- classes/cTable.lua
+++ classes/cTable.lua
@@ -221,3 +221,15 @@ function cTable.bounds(t)
 
 end
 
+---------------------------------------------------------------------------------------------------
+-- Check if a given table is indexed (exclusively with numerical indices)
+
+function cTable.is_indexed(t)
+  local i = 0
+  for _ in pairs(t) do
+    i = i + 1
+    if t[i] == nil then return false end
+  end
+  return true
+end
+
